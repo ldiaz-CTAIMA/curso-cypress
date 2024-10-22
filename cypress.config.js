d8a70591699d8aa5768891deb46b04e3b0ca365d
Lara Diaz <laliclohesey@gmail.com>
2024-10-22 15:14:09 +0200

    Fix report and flakyness

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 const { configureAllureAdapterPlugins } = require('@mmisty/cypress-allure-adapter/plugins');
 
 module.exports = defineConfig({
+  retries: 1,
   projectId: 'b36osx',
   e2e: {
     setupNodeEvents(on, config) {
