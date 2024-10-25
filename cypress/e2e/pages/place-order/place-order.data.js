1236a470728f9116b41b924c85cd2f7caf64a3f2
Lara Diaz <laliclohesey@gmail.com>
2024-10-25 12:57:11 +0200

    Creacion de la apgina Place order

---
+++ cypress/e2e/pages/place-order/place-order.data.js
@@ -0,0 +1,12 @@
+export class PlaceOrderData{
+    static get testDate(){
+        return{
+            name: 'Test name',
+            country: 'United States',
+            city: 'Boston',
+            creditCardNumber: '4111 1111 1111 1111',
+            month: 12,
+            year: 2024,
+        }
+    }
+}
