51df27fd337d43a774d566f1555677a25b3dcbe4
Lara Diaz <laliclohesey@gmail.com>
2024-10-30 16:01:37 +0100

    Crear usuario aleatorio

--- cypress/e2e/tests/registro-y-autenticacion.cy.js
+++ cypress/e2e/tests/registro-y-autenticacion.cy.js
@@ -2,6 +2,8 @@ import { CommonPageData } from "../pages/common-page/common-page.data";
 import { CommonPageMethods } from "../pages/common-page/common-page.methods";
 import { SingupMethods } from "../pages/singup/singup.methods";
 import { Logger } from "../utilitarios/logger";
+const user = CommonPageMethods.generateRandomString();
+const password = CommonPageMethods.generateRandomString(7);
 
 describe(CommonPageData.testSuites.registroYAutenticacion, () => {
     it("Registro de usuario vádido", () => {
@@ -15,8 +17,8 @@ describe(CommonPageData.testSuites.registroYAutenticacion, () => {
 
         Logger.stepNumber(3)
         Logger.step('Completar todos los campos obligatorios con información válida')
-        SingupMethods.insertUsername('UsernameUnicoNoRegistradoAun1')
-        SingupMethods.insertPassword('PasswordUnicaNoRegistradaAun1')
+        SingupMethods.insertUsername((user))
+        SingupMethods.insertPassword((password))
         
         Logger.stepNumber(4)
         Logger.step('Hacer clic en "Sign up" para registrar el usuario')
