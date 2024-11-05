184b6c7af8b99e232e1f6e403ed0214ebfafc1c1
Lara Diaz <laliclohesey@gmail.com>
2024-11-05 15:24:06 +0100

    Caso de pruebas "Inicio de sesión inválido"

--- cypress/e2e/tests/autenticacion.cy.js
+++ cypress/e2e/tests/autenticacion.cy.js
@@ -6,23 +6,44 @@ import { Logger } from "../utilitarios/logger";
 
 describe(CommonPageData.testSuites.autenticacion, ()=>{
     it('Inicio de sesión válido', ()=>{
-        Logger.stepNumber(1)
-        Logger.step('Navegar a la página de inicio')
+        Logger.stepNumber(1);
+        Logger.step('Navegar a la página de inicio');
         CommonPageMethods.navigateToDemoBlaze();
 
-        Logger.stepNumber(2)
-        Logger.step('Hacer clic en "Log in" en la barra de navegación')
+        Logger.stepNumber(2);
+        Logger.step('Hacer clic en "Log in" en la barra de navegación');
         CommonPageMethods.clickOnLoginOption();
 
-        Logger.stepNumber(3)
-        Logger.step('Ingresar un nombre de usuario y contraseña válidos')
-        LoginMethods.insertUsername(LoginData.validCredentials.username)
-        LoginMethods.insertPassword(LoginData.validCredentials.password)
+        Logger.stepNumber(3);
+        Logger.step('Ingresar un nombre de usuario y contraseña válidos');
+        LoginMethods.insertUsername(LoginData.validCredentials.username);
+        LoginMethods.insertPassword(LoginData.validCredentials.password);
 
-        Logger.stepNumber(4)
-        Logger.step('Hacer clic en "Log in" para iniciar sesión')
+        Logger.stepNumber(4);
+        Logger.step('Hacer clic en "Log in" para iniciar sesión');
         LoginMethods.clickOnLoginButton();
-        Logger.verification('Verificar que se redirige al usuario a la página de inicio')
+        Logger.verification('Verificar que se redirige al usuario a la página de inicio');
         CommonPageMethods.verifySignedUser(LoginData.validCredentials.username);
+    })
+
+    it('Inicio de sesión inválido', ()=>{
+        Logger.stepNumber(1);
+        Logger.step('Navegar a la página de inicio');
+        CommonPageMethods.navigateToDemoBlaze();
+
+        Logger.stepNumber(2);
+        Logger.step('Hacer clic en "Log in" en la barra de navegación');
+        CommonPageMethods.clickOnLoginOption();
+
+        Logger.stepNumber(3);
+        Logger.step('Ingresar un nombre de usuario y/o contraseña inválidos');
+        LoginMethods.insertUsername(LoginData.validCredentials.username);
+        LoginMethods.insertPassword('contrasenainvalida');
+
+        Logger.stepNumber(4);
+        Logger.step('Hacer clic en "Log in" para iniciar sesión');
+        LoginMethods.clickOnLoginButton();
+        Logger.verification('Verificar que se muestra un mensaje de error indicando que el inicio de sesión ha fallado');
+        LoginMethods.verifyWrongPasswordMessage();
     })
 })
